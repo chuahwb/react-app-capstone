1c8fd90bd325c2a8675a9c986ac57c50185908f4
chuahwb <chuahwb@gmail.com>
2023-02-06 15:18:46 +0800

    Added pages and HomePage components

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import logo from '../Assets/Logo.svg'
 
 const Header = () => {
-  return (
-    <header>
-        <img src={logo} alt='logo' />
-    </header>
-  )
+    return (
+        <nav>
+            <ul>
+                <li><a href='#home'>Home</a></li>
+                <li><a href='#about'>About</a></li>
+                <li><a href='#menu'>Menu</a></li>
+                <li><a href='#reservations'>Reservations</a></li>
+                <li><a href='#orderonline'>Order Online</a></li>
+                <li><a href='#login'>Login</a></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Header
---
+++ src/pages/BookingPage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../components/Header'
+import IntroSection from '../components/IntroSection'
+import MenuSection from '../components/MenuSection'
+import TestimonialSection from '../components/TestimonialSection'
+import AboutSection from '../components/AboutSection'
+import Footer from '../components/Footer'
+
+const BookingPage = () => {
+  return (
+    <>
+        <Header/>
+        <IntroSection/>
+        <MenuSection/>
+        <TestimonialSection/>
+        <AboutSection/>
+        <Footer/>
+    </>
+  )
+}
+
+export default BookingPage
