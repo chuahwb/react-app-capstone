553526aa6d890dc2491b009fbac2fa1c160cd554
chuahwb <chuahwb@gmail.com>
2023-02-09 00:46:40 +0800

    updated Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import BookingPage from './pages/BookingPage';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <HomePage/>
-    </>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/reservations' element={<BookingPage />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import logo from '../assets/Logo.svg'
-import { Link, Route, Routes } from 'react-router-dom'
-import HomePage from '../pages/HomePage';
-import BookingPage from '../pages/BookingPage';
+import { Link } from 'react-router-dom'
+import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll'
+import '../App.css'
 
 const Header = () => {
     return (
-        <>
+        <div className='hero'>
             <img src={logo} alt='logo' />
             <nav>
-                <Link to="/" className="nav-item">Home</Link>
-                <a href='#about'>About</a>
-                <a href='#menu'>Menu</a>
-                <Link to="/reservations" className="nav-item">Reservations</Link>
-                <a href='#orderonline'>Order Online</a>
-                <a href='#login'>Login</a>
+                <ul>
+                    <li className="nav-item"><Link to="/" id='link'>Home</Link></li>
+                    <li className="nav-item"><ScrollLink activeClass="active" smooth spy to="about">About</ScrollLink></li>
+                    <li className="nav-item"><ScrollLink activeClass="active" smooth spy to="menu">Menu</ScrollLink></li>
+                    <li className="nav-item"><Link to="/reservations" id='link'>Reservations</Link></li>
+                    <li className="nav-item"><ScrollLink activeClass="active" smooth spy to="orderonline">Order Online</ScrollLink></li>
+                    <li className="nav-item"><ScrollLink activeClass="active" smooth spy to="login">Login</ScrollLink></li>
+                </ul>
             </nav>
-            <Routes>
-                <Route path='/' element={<HomePage />}></Route>
-                <Route path='/reservations' element={<BookingPage />}></Route>
-            </Routes>
-        </>
+        </div>
     )
 }
 
