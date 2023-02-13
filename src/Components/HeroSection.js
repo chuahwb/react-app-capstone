3b0747c8693ebd06aa370a51d17586118d34e778
chuahwb <chuahwb@gmail.com>
2023-02-13 14:47:41 +0800

    updated hero section

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -1,25 +1,30 @@
 import React from 'react'
 import '../App.css'
 import { Link } from 'react-router-dom'
+import restaurantFood from '../assets/restauranfood.jpg'
 
 const HeroSection = () => {
   return (
+    <div className='section-hero'>
     <div className='wrapper'>
-      <div className='container grid grid-two-column'>
-        <div className='section-hero-data'>
-          <h1 className='hero-title'>Little Lemon</h1>
-          <h2 className='hero-subtitle'>Chicago</h2>
-          <p className='hero-para'>We are a family owned
-            Mediterranean restaurant,
-            focused on traditional
-            recipes served with a modern
-            twist.</p>
-        </div>
+      <div className='section-hero-data'>
+        <h1 className='hero-title'>Little Lemon</h1>
+        <h2 className='hero-subtitle'>Chicago</h2>
+        <p className='hero-para'>We are a family owned
+          Mediterranean restaurant,
+          focused on traditional
+          recipes served with a modern
+          twist.</p>
         <button className='btn reserve-a-table-btn'>
           <Link to="/reservations">Reserve a Table</Link>
         </button>
-        <div className='section-hero-image'></div>
       </div>
+      <div className='section-hero-image'>
+        <picture>
+          <img src={restaurantFood} alt='hero-restaurant-food' className='hero-image' />
+        </picture>
+      </div>
+    </div>
     </div>
   )
 }
