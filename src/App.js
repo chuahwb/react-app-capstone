c1dae2bbc2be6ddeef5fda18fa3f9d1de79225b7
chuahwb <chuahwb@gmail.com>
2023-02-15 17:39:31 +0800

    added code splitting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
-import Navbar from './components/Navbar'
-import HomePage from './pages/HomePage';
-import ReservationsPage from './pages/ReservationsPage';
-import OrderPage from './pages/OrderPage';
 import { Route, Routes } from 'react-router-dom'
+import { lazy } from 'react'
+import Navbar from './components/Navbar';
+
+const HomePage = lazy(() => import("./pages/HomePage"))
+const ReservationsPage = lazy(() => import("./pages/ReservationsPage"))
+const OrderPage = lazy(() => import("./pages/OrderPage"))
 
 function App() {
   return (
     <>
-      <Navbar />
       <div className='container'>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/reservations' element={<ReservationsPage />} />
-          <Route path='/order-online' element={<OrderPage />} />
+          <Route path='/' element={<Navbar />}>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/reservations' element={<ReservationsPage />} />
+            <Route path='/order-online' element={<OrderPage />} />
+          </Route>
         </Routes>
       </div>
     </>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,22 +2,29 @@ import React from 'react'
 import logo from '../assets/Logo.svg'
 import '../App.css'
 import CustomLink from './CustomLink'
+import { Outlet } from 'react-router-dom'
+import { Suspense } from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <header>
-                <img src={logo} alt='nav-logo' className='nav-logo' />
-            </header>
-            <ul>
-                <CustomLink to="/" className='nav-item'>Home</CustomLink>
-                <li><a href='#about' className='nav-item'>About</a></li>
-                <li><a href='#menu' className='nav-item'>Menu</a></li>
-                <CustomLink to="/reservations" className='nav-item'>Reservations</CustomLink>
-                <CustomLink to="/order-online" className='nav-item'>Order Online</CustomLink>
-                <li><a href='#login' className='nav-item'>Login</a></li>
-            </ul>
-        </nav>
+        <>
+            <nav className='nav'>
+                <header>
+                    <img src={logo} alt='nav-logo' className='nav-logo' />
+                </header>
+                <ul>
+                    <CustomLink to="/" className='nav-item'>Home</CustomLink>
+                    <li><a href='#about' className='nav-item'>About</a></li>
+                    <li><a href='#menu' className='nav-item'>Menu</a></li>
+                    <CustomLink to="/reservations" className='nav-item'>Reservations</CustomLink>
+                    <CustomLink to="/order-online" className='nav-item'>Order Online</CustomLink>
+                    <li><a href='#login' className='nav-item'>Login</a></li>
+                </ul>
+            </nav>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <Outlet />
+            </Suspense>
+        </>
     )
 }
 
