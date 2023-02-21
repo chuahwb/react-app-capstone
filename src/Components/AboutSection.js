f75e1881e7384e7f9844bc2e478b0b465f6c7174
chuahwb <chuahwb@gmail.com>
2023-02-22 00:15:47 +0800

    updated about section

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import '../App.css'
+import chefA from '../assets/MarioandAdrianA.jpg'
+import chefB from '../assets/restaurantchefB.jpg'
 
 const AboutSection = () => {
     return (
-        <section id='about'>
-            ABOUT
-        </section>
+        <div className='section-about' id='about'>
+            <div className='wrapper'>
+                <div className='section-about-main'>
+                    <h2>Little Lemon</h2>
+                    <h3>Chicago</h3>
+                    <p>We believe in warm hospitality and that good food should be shared. Our menu is created with a focus on cooking with intention & instinct. It`s contemporary and honest cooking, inspired by the joys of comfort food.</p>
+                    <img src={chefB} alt='chefB' className='chefB' />
+                    <img src={chefA} alt='chefA' className='chefA' />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/Components/CardTestimonial.js
+++ src/Components/CardTestimonial.js
@@ -5,15 +5,13 @@ import { faStar } from '@fortawesome/free-solid-svg-icons'
 const CardTestimonial = ({ name, rating, review, imgSrc }) => {
   return (
     <div className='card-testimonial' >
-      <div className='testimonial-header'>
-        <p className='ratings'>{rating}</p>
-        <FontAwesomeIcon icon={faStar} className='ratings-icon' />
+      <div className='ratings'>
+        <span className='testimonial-ratings'>{rating}</span>
+        <span><FontAwesomeIcon icon={faStar} className='testimonial-ratings-icon' /></span>
       </div>
-      <div className='testimonial-content'>
-        <img src={imgSrc} alt={name} className='testimonial-avatar' />
-        <h3 className='testimonial-avatar-name'>{name}</h3>
-        <p className='testimonial-review'>{review}</p>
-      </div>
+      <img src={imgSrc} alt={name} className='avatar testimonial-avatar' />
+      <h3 className='avatar testimonial-avatar-name'>{name}</h3>
+      <p className='testimonial-review'>{review}</p>
     </div>
   )
 }
