aa11531e53a212e1231bc7928dc44eb3ff185359
chuahwb <chuahwb@gmail.com>
2023-02-22 11:46:17 +0800

    updated footer section

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,44 @@
 import React from 'react'
 import '../App.css'
+import logo from '../assets/Logo.svg'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import CustomLink from './CustomLink';
 
 const Footer = () => {
   return (
     <footer>
-      <section id='footer'>
-        Footer
-      </section>
+      <div className='section-footer'>
+        <div className='wrapper'>
+          <div className='section-footer-main'>
+            <img src={logo} alt='footer logo' className='footer-logo' />
+            <div className='footer-nav-category'>
+              <h4 className='category-heading'>Doormat Navigations</h4>
+              <CustomLink to="/" className='footer-nav-item'>Home</CustomLink>
+              <a href='#about' className='footer-nav-item'>About</a>
+              <a href='#menu' className='footer-nav-item'>Menu</a>
+              <CustomLink to="/reservations" className='footer-nav-item'>Reservations</CustomLink>
+              <CustomLink to="/order-online" className='footer-nav-item'>Order Online</CustomLink>
+              <a href='#login' className='footer-nav-item'>Login</a>
+            </div>
+            <div className='footer-nav-category'>
+              <h4 className='category-heading'>Contact</h4>
+              <h5>Address</h5>
+              <h5>Phone Number</h5>
+              <h5>Email</h5>
+            </div>
+            <div className='footer-nav-category'>
+              <h4 className='category-heading'>Social Media Links</h4>
+              <div className='footer-icons'>
+                <InstagramIcon />
+                <FacebookIcon />
+                <TwitterIcon />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
