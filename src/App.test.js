8f4f767a481601414d33e897616ed01839fa38b2
chuahwb <chuahwb@gmail.com>
2023-03-10 01:46:38 +0800

    unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ReservationSection from './components/ReservationSection'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Renders the Reservation Secion heading', () => {
+  render(<ReservationSection />);
+  const headingElement = screen.getByText("Reservation");
+  expect(headingElement).toBeInTheDocument();
+})
